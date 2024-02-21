1b62270d50c1c00cb70cc20b347a0b0a9edcd3d0
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-02-21 17:48:59 -0300

    .

--- lista-aluno.php
+++ lista-aluno.php
@@ -13,8 +13,8 @@
         <header>
             <img src="img.jpg" alt="giannis" />
             <nav>
-                <a href="aluno.html">Aluno</a>
                 <a href="professor.html">Professor</a>
+                <a href="aluno.html">Aluno</a>
             </nav>
             <img src="img.jpg" alt="giannis" />
         </header>
@@ -32,7 +32,7 @@
                 $idade = $_GET["idade"];
                 $senha = $_GET["senha"];
                 $endereco = $_GET["endereço"];
-                
+
 
                 echo "<td>$nome</td>";
                 echo "<td>$idade</td>";
